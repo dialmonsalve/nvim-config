f88c1f20d4d9bfa26d28bdbed82877796552d77c
Diego Monsalve <diales1981@gmail.com>
2024-08-04 20:03:56 -0500

    Update cmp config

--- lua/dialmonsalve/plugins/ui/nvim-cmp.lua
+++ lua/dialmonsalve/plugins/ui/nvim-cmp.lua
@@ -1,26 +1,35 @@
+---@diagnostic disable: missing-fields
 return {
 	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
+	event = "VeryLazy",
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/cmp-git",
+		"windwo/nvim-autopairs",
 		{
 			"L3MON4D3/LuaSnip",
 			-- follow latest release.
 			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 			-- install jsregexp (optional!).
 			build = "make install_jsregexp",
+			config = true,
 		},
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
 		"onsails/lspkind.nvim", -- vs-code like pictograms
 	},
+
 	config = function()
 		local cmp = require("cmp")
 
 		local luasnip = require("luasnip")
 
 		local lspkind = require("lspkind")
+
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
 		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
 		require("luasnip.loaders.from_vscode").lazy_load()
@@ -35,10 +44,19 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<C-n>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.choice_active() then
+						luasnip.change_choice(1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
@@ -66,6 +84,27 @@ return {
 					return item
 				end,
 			},
+		})
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+
+		cmp.setup.filetype("gitcommit", {
+			sources = cmp.config.sources({
+				{ name = "git" },
+			}, { name = "buffer" }),
+		})
+
+		cmp.setup.cmdline({ "/", "?" }, {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, { name = "cmdline" }),
 		})
 	end,
 }
